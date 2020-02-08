2bdaa21805d638e7dc7f866e84e0e78ef842b841
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-02-08 08:09:18 +0100

    test for user profile creation

--- decagoncenter/centerapi/tests.py
+++ decagoncenter/centerapi/tests.py
@@ -1,6 +1,48 @@
 from django.test import TestCase
+from .models import User
+from rest_framework import status
+from django.urls import reverse
 
 # Create your tests here.
-class TestAPI(TestCase):
-    def test_integer(self):
-        self.assertEqual(2, 2)
+class TestModel(TestCase):
+    def setUp(self):
+        self.user_name = 'new_user'
+        self.user_username = 'okanlomo'
+        self.user_email = 'foo@example.com'
+        self.user_password = 'kolap129'
+        self.user_position = 'Lead Trainer'
+        self.user = User(fullname = self.user_name, username = self.user_username, email = self.user_email, 
+            password = self.user_password, position = self.user_position)
+
+    def test_successful_bucket_create(self):
+        self.first_count = User.objects.count()
+        self.user.save()
+        self.count_again = User.objects.count()
+        
+        self.assertNotEqual(self.first_count, self.count_again)
+
+class TestView(TestCase):
+    def test_successful_post(self):
+        data = {'fullname':'new_user', 'username':'okanlomo', 'email':'foo@example.com', 'password':'kolap129', 'position':'Lead Trainer'}
+        url = reverse('create')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_unsuccessful_post_invalid_password_supplied(self):
+        data = {'fullname':'new_user', 'username':'okanlomo', 'email':'foo@example.com', 'password':'kolap1290', 'position':'Lead Trainer'}
+        url = reverse('create')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unsuccessful_post_password_not_supplied(self):
+        data = {'fullname':'new_user', 'username':'okanlomo', 'email':'foo@example.com', 'position':'Lead Trainer'}
+        url = reverse('create')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unsuccessful_post_position_not_supplied(self):
+        data = {'fullname':'new_user', 'username':'okanlomo', 'email':'foo@example.com', 'password':'kolap129'}
+        url = reverse('create')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)

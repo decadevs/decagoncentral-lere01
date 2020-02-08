c22c4ad8dc3fb84bdae65f2683685b69e8cf972a
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-02-08 08:07:25 +0100

    adjust model to enforce some validation

--- decagoncenter/centerapi/models.py
+++ decagoncenter/centerapi/models.py
@@ -3,11 +3,11 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class User(models.Model):
-    fullname = models.CharField(max_length = 200)
-    username= models.CharField(max_length = 50, unique = True)
-    email = models.EmailField()
-    password = models.CharField(max_length=8, validators=[MinLengthValidator(8)], unique = True)
-    position = models.CharField(max_length = 50)
+    fullname = models.CharField(max_length = 200, blank = False)
+    username= models.CharField(max_length = 50, unique = True, blank = False)
+    email = models.EmailField(blank = False)
+    password = models.CharField(max_length=8, validators=[MinLengthValidator(8)], unique = True, blank = False)
+    position = models.CharField(max_length = 50, blank = False)
 
     def __str__(self):
         return self.name

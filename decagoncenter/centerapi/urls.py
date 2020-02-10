03275d5f9267f6aba3c83d9c3718826abedc5fe4
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-02-10 10:12:58 +0100

    ...

--- decagoncenter/centerapi/urls.py
+++ decagoncenter/centerapi/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import Users
+from .views import CreateUser, GetUser
+from rest_framework.authtoken.views import obtain_auth_token 
 
 urlpatterns = [
-    path('create_user/', Users.as_view(), name = 'create'),
+    path('create_user/', CreateUser.as_view(), name = 'create'),
+    path('get_user/', GetUser.as_view(), name = 'request'),
+    path('api-token-auth/', obtain_auth_token, name = 'api-token-auth'),
 ]
--- decagoncenter/centerapi/views.py
+++ decagoncenter/centerapi/views.py
@@ -5,9 +5,11 @@ from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
-class Users(APIView):
+class CreateUser(APIView):
 
     def get(self, request, format='json'):
         user = User.objects.all()
@@ -23,3 +25,33 @@ class Users(APIView):
             return Response(serialize.data, status = status.HTTP_201_CREATED)
 
         return Response(serialize.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class GetUser(APIView):
+    # def get(self, request, email, password, format='json'):
+    #     user = User.objects.all()
+    #     serializer = Serializer(user, many=True)
+    #     return Response('Successful', status = status.HTTP_200_OK)
+
+    def post(self, request):
+    
+        data = request.data
+
+        try:
+            username = data['username']
+            password = data['password']
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(username=username, password=password)
+        except:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            user_token = user.auth_token.key
+        except:
+            user_token = Token.objects.create(user=user)
+
+        data = {'token': user_token}
+        return Response(data=data, status=status.HTTP_200_OK)
